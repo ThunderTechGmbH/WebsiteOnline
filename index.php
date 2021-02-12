5094c66d53bae9e487a6da8e2a6ecb7f9611ded5
Project Awake <77062263+ProjectAsleep@users.noreply.github.com>
2021-02-12 10:57:07 +0100

    Creation

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="main.js" defer></script>
+        <title>Contact Us</title>
+    </head>
+    <body>
+    <div class="loader_bg">
+  <div class="loader"></div>
+</div>
+
+<div class="line">
+  <hr>
+</div>
+    <header class="header">
+        <div href="#">
+            <img src="Flash.png" alt="Pic" height="50px" width="50px" >
+        </div>
+      <div class="menu-btn">
+        <div class="menu-btn__lines"></div>
+      </div>
+      <ul class="menu-items">
+        <li><a href="index.html">Startseite</a></li>
+        <li><a href="firma.html">Firma</a></li>
+        <li><a href="produkte.html">Produkte</a></li>
+        <li><a href="impressum.html">Impressum</a></li></li>
+      </ul>
+    </header>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script>
+          setTimeout(function(){
+            $('.loader_bg').fadeToggle();
+          }, 500);
+        </script>
+        <main>
+        <section class="first-section">
+            <div class="first-sec">
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <div class="Contact login-wrapper">
+            <form class="contact-form" action="contactform.php" method="post">
+              <div class="input-group">
+                <input type="text" name="name" placeholder="Full Name">
+                </div>
+                <div class="input-group">
+                <input type="text" name="mail" placeholder="Your E-mail">
+                </div>
+                <div class="input-group">
+                <input type="text" name="subject" placeholder="Subject">
+                </div>
+                <div class="input-group">
+                <textarea name="message" placeholder="Message"></textarea>
+                </div>
+                <button type="submit" name="submit" id="buttonS">Send</button>
+                </div>
+                </div>
+            </section>
+            </form>
+        </main>
+    </body>
+</html>
---
+++ main.js
@@ -0,0 +1,152 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuItems = document.querySelector(".menu-items");
+const card = document.querySelector(".card__inner");
+const card1 = document.querySelector(".card__inner1");
+const card2 = document.querySelector(".card__inner2");
+const card3 = document.querySelector(".card__inner3");
+const fader = document.querySelectorAll(".fade-in");
+// First Card
+const Text = document.querySelector(".centeredText");
+const Foto = document.querySelector(".item-fade");
+// Second Card
+const Text1 = document.querySelector(".centeredText1");
+const Foto1 = document.querySelector(".item-fade1");
+// Third Card 
+const Text2 = document.querySelector(".centeredText2");
+const Foto2 = document.querySelector(".item-fade2");
+// Fourth Card
+const Text3 = document.querySelector(".centeredText3");
+const Foto3 = document.querySelector(".item-fade3");
+
+
+
+
+let menuOpen = false;
+
+window.onload = function () {
+    window.scrollTo(0, 0);
+  }
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    menuItems.classList.toggle('open');
+  
+});
+
+  card.addEventListener("click", function (e) {
+    card.classList.toggle('is-flipped');
+  });
+
+  card1.addEventListener("click", function (e) {
+    card1.classList.toggle('is-flipped1');
+  });
+
+  card2.addEventListener("click", function (e) {
+    card2.classList.toggle('is-flipped2');
+  });
+
+  card3.addEventListener("click", function (e) {
+    card3.classList.toggle('is-flipped3');
+  });
+
+  const appearOptions = {
+      threshhold: 1
+  };
+
+  const appearOnScroll = new IntersectionObserver
+  (function(
+    entries,
+    appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    }, 
+    appearOptions);
+
+    fader.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+
+// Card 1
+    card.addEventListener("mouseover", function (e) {
+      Text.classList.toggle('disappear');
+      Foto.classList.toggle('show');
+      console.log ("Mouseenter");
+    });
+
+    card.addEventListener("mouseout", function (e) {
+      Text.classList.toggle('disappear');
+      Foto.classList.toggle('show');
+      console.log ("Mousleave");
+    });
+// Card 2
+    card1.addEventListener("mouseover", function (e) {
+      Text1.classList.toggle('disappear1');
+      Foto1.classList.toggle('show1');
+      console.log ("Mouseenter");
+    });
+
+    card1.addEventListener("mouseout", function (e) {
+      Text1.classList.toggle('disappear1');
+      Foto1.classList.toggle('show1');
+      console.log ("Mousleave");
+    });
+// Card 3
+card2.addEventListener("mouseover", function (e) {
+  Text2.classList.toggle('disappear2');
+  Foto2.classList.toggle('show2');
+  console.log ("Mouseenter");
+});
+
+card2.addEventListener("mouseout", function (e) {
+  Text2.classList.toggle('disappear2');
+  Foto2.classList.toggle('show2');
+  console.log ("Mousleave");
+});
+// Card 3
+card3.addEventListener("mouseover", function (e) {
+  Text3.classList.toggle('disappear3');
+  Foto3.classList.toggle('show3');
+  console.log ("Mouseenter");
+});
+
+card3.addEventListener("mouseout", function (e) {
+  Text3.classList.toggle('disappear3');
+  Foto3.classList.toggle('show3');
+  console.log ("Mousleave");
+});
+
+
+    function visitPage(){
+        window.location='index.php';
+    }
+  
+
+    window.addEventListener('resize', function(e){
+
+      var width = {
+        width: window.innerWidth || document.body.clientWidth,
+      }
+
+      const widthNum = (Object.values(width))
+
+      var element = document.getElementById('card'),
+      style = window.getComputedStyle(element),
+      margin = style.getPropertyValue('margin'); 
+
+      console.log(widthNum);
+
+       if (widthNum > 960) {
+         if (widthNum > widthNum - 1) {
+          console.log (margin)
+         }
+
+      }
+      });
+    
